ae0cf30f8d18aea4af246ec050e3a8f343679623
Jesus Manteca <jesusmanteca@gmail.com>
2020-08-16 17:07:44 -0500

    final touches

--- assets/script.js
+++ assets/script.js
@@ -18,70 +18,91 @@ var numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 
 var initiateProgram = function () {
     // establish the main thing... length of password
-    var userPromptLength = window.prompt("How long do you want to your password? Has to be between 8 and 120 characters.")
+    var userPromptLength = window.prompt("How long do you want your password? (8 and 120 characters)")
     // parseInt makes it into a digit
     userPromptLength = parseInt(userPromptLength);
     // console.log(userPromptLength)
 
+
+
+    // this will hold the possible range of possibilities
     var emptyArray = [];
+    var passwordArray = []
 
+
+    // 2. questionSeries is a function to fill the emptyArray bucket by asking a series of questions that will bring in a range of possibilities 
     var questionSeries = function () {
-        
-        var userPromptSmallCase = window.confirm("Do you want Small Case?")
-        if (userPromptSmallCase) {
-            emptyArray = emptyArray.concat(smallCase);
-            // add smallCase array into password array parameters
-        }
-    
-        var userPromptUpperCase = window.confirm("Do you want Upper Case?")
-        if (userPromptUpperCase) {
-            emptyArray = emptyArray.concat(upperCase);
-        }
-    
-        var userPromptSpecialCharacters = window.confirm("Do you want Special Characters?")
-        if (userPromptSpecialCharacters) {
-            emptyArray = emptyArray.concat(specialCharacters)
-        }
-    
-        var userPromptNumberChoices = window.confirm("Do you want Numbers?")
-        if (userPromptNumberChoices) {
-            emptyArray = emptyArray.concat(numberChoices)
+
+        var questions = function(){
+            var userPromptSmallCase = window.confirm("Do you want Small Case?")
+            var userPromptUpperCase = window.confirm("Do you want Upper Case?")
+            var userPromptSpecialCharacters = window.confirm("Do you want Special Characters?")
+            var userPromptNumberChoices = window.confirm("Do you want Numbers?")
+            if (userPromptSmallCase) {
+                emptyArray = emptyArray.concat(smallCase);
+            } if (userPromptUpperCase) {
+                emptyArray = emptyArray.concat(upperCase);
+            } if (userPromptSpecialCharacters) {
+                emptyArray = emptyArray.concat(specialCharacters)
+            } if (userPromptNumberChoices) {
+                emptyArray = emptyArray.concat(numberChoices)
+            }
         }
 
-    
-        console.log("Possibilites selected:", emptyArray);
-        console.log("emptyArray length:", emptyArray.length);
-        
-        var passwordArray = []
-        
-    
-        //choose userPromptLength number of characters at random from that list and return it
+        // Once the variable is introduced, then you execute the function
+        questions();
+
+        // if after you execute the questions function the bucket is stil as empty as with what you started, execute the questions function again until it has something in it
+        if (emptyArray.length === 0){
+            window.alert("Choose something, let's try it again, yeah?")
+            questions();
+        }
+
+        // do a for loop - a function that runs a number of times as long as the size of our password. We want every digit to be randomly selected. 
         for (i = 0; i < userPromptLength; i++) {
 
-            var magicBallNumber = Math.floor(Math.random()*emptyArray.length);
-            // console.log("This is the index number: ", magicBallNumber);
+            // I call this the Magic Ball Number - it will pick a number at random from 0 to 1 and multiply it by as many items in the bucket - each item gets an equal opportunity to be selected. The number gets rounded down so it can start at 0 since that's the first index and indexes come in whole numbers. 
+            var magicBallNumber = Math.floor(Math.random() * emptyArray.length);
 
+            // After matching the Magic Ball Number to an actual item from the bucket by using the index spot that matches the ball, you get a character. 
             var magicBallCharacter = emptyArray[magicBallNumber];
-            // console.log("This is the character indexed: ", magicBallCharacter);
 
+            // Find out more about push **vs** concat
+            // Everytime the loop goes around, a different character will emerge which will be then be placed in the actual password array. It'll do so into an array of different numbers but that's okay, there's a way to combine all of it. 
             passwordArray = passwordArray.concat(magicBallCharacter)
-            
-        }
 
-        console.log("Password Array: " + passwordArray.join(""))
-
-        // window.alert("Your super-secret " + userPromptLength + " digit password is:" + "\n" + "\n" + passwordArray.join(""))
-
-        document.getElementById("password").innerHTML = "Your super-secret " + userPromptLength + " digit password is:" + "\n" + "\n" + passwordArray.join("")
+        }// Now we have a filled Password Array
+        
+        
+        console.log("Possibilites selected \n Items in the Bucket:", emptyArray);
     }
-    
-    if (userPromptLength >= 8 && userPromptLength <= 128) {
+
+    // 1. This starts the magic after the variable is already defined. At the end of the questionSeries we'll have our emptyArray bucket  and our passwordArray bucket filled
+    if (userPromptLength >= 8 && userPromptLength <= 120) {
         questionSeries();
     }
     else {
         window.alert("Give me something to work here, choose between 8 and 120, please.");
-        initiateProgram();
+        this.initiateProgram();
     };
+
+    //This outputs the html with the stuff (new password) from the passwordArray bucket after it formats it all into string
+    var newPasswordArray
+    var generatePassword = function () {
+        // getting the html id and passing through it the actual password message
+        document.getElementById("password").innerHTML = "Your super-secret " + userPromptLength + " digit password is: " + passwordArray.join("")
+    }
+
+    newPasswordArray = passwordArray.join("")
+    console.log("Password Array: " + newPasswordArray)
+    generatePassword();
+
+
+    // if (emptyArray.length === 0) {
+    //     questionSeries();
+    // }
+
+
 
 }
 
@@ -144,7 +165,7 @@ var initiateProgram = function () {
 //     console.log("Random index chosen:", passwordRound1)
 //     var passwordReturn = concat()[passwordRound1]
 //     console.log("Symbol for passcode number", [i + 1],"is", passwordReturn)
-    
+
 // }
 
 
